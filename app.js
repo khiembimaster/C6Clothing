622867baa121c67714eff93616a9411782006e40
trong <lehuutrong141@gmail.com>
2023-12-09 13:33:57 +0700

    model

--- app.js
+++ app.js
@@ -3,39 +3,34 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var app = express();
-
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+
+const db = require('./models/db');
+const Product = require('./models/product.m');
+
+
+app.get('/', async function(req,res,next) {
+  console.log("abc");
+  const rs = await db.filterByRange("Products","Price",300000.0000,1600000000.0000,1,5);
+  console.log(rs);
+  next();
+})
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
