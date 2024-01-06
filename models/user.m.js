8ffcd4897f465e87faec21860326dee0de32730b
21127360 <pvminh21@clc.fitus.edu.vn>
2024-01-06 13:32:49 +0700

    Update UI

--- models/user.m.js
+++ models/user.m.js
@@ -1,33 +1,33 @@
 const db = require('./db');
 const tbName = 'Users';
-module.exports = class Account{
-    constructor(username, password, name, email){
+module.exports = class Account {
+    constructor(username, password, name, email) {
         this.Username = username;
         this.Password = password;
         this.Name = name;
         this.Email = email;
         this.Permission = 1;
     }
-    static async All(page,perPage){
-        const rs = await db.findAll(tbName,page,perPage);
+    static async All(page, perPage) {
+        const rs = await db.findAll(tbName, page, perPage);
         return rs;
     }
-    static async Add(user){
+    static async Add(user) {
         const rs = await db.add(tbName, user);
         return rs;
     }
-    static async Get(username){
+    static async Get(username) {
         const rs = await db.findOne(tbName, 'Username', username);
         return rs;
     }
-    static async Del(username){
+    static async Del(username) {
         const rs = await db.del(tbName, 'Username', username);
         return rs;
     }
-    static async Update(user){
+    static async Update(user) {
         const rs = await db.update(tbName, user);
         return rs;
     }
-    
-    
+
+
 }
