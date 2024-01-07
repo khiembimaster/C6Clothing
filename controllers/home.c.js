c80dc5af15cfbc7183319c4933641f95c4284002
21127317 <nhkhiem21@clc.fitus.edu.vn>
2024-01-07 15:05:40 +0700

    update categories section

--- controllers/home.c.js
+++ controllers/home.c.js
@@ -1,14 +1,15 @@
-
+const Category = require('../models/category.m');
 module.exports = {
-    homePage: (req, res, next) => {
+    homePage: async (req, res, next) => {
         try {
             let user = null;
             if(req.session.passport){
                 user = req.session.passport.user
             }
-            
+            const categories = await Category.All();
             res.render('homepage', {
                 'user':  user,
+                categories: categories,
                 css:()=>'css/homepage',
                 js:()=>'js/empty'
             })
