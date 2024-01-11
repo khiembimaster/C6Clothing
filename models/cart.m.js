e21a1e8a4950c6362afe411a0a7948b0e8c9c02f
21127360 <pvminh21@clc.fitus.edu.vn>
2024-01-12 00:08:14 +0700

    nothing

--- models/cart.m.js
+++ models/cart.m.js
@@ -1,33 +1,33 @@
 const db = require('./db');
 const tbName = 'Carts';
 const CartItemDel = require('./cartItems.m').Del
-module.exports = class Cart{
-    constructor(UserID,Total){
-      this.UserID = UserID;
-      this.Total = Total;
+module.exports = class Cart {
+    constructor(UserID, Total) {
+        this.UserID = UserID;
+        this.Total = Total;
     }
-    static async All(page,perPage){
-        const rs = await db.findAll(tbName,page,perPage);
+    static async All(page, perPage) {
+        const rs = await db.findAll(tbName, page, perPage);
         return rs;
     }
-    static async Add(order){
+    static async Add(order) {
         const rs = await db.add(tbName, order);
         return rs;
     }
-    static async Get(id){
+    static async Get(id) {
         const rs = await db.findOne(tbName, 'ID', id);
         return rs;
     }
-    static async DelByUserID(userID){
+    static async DelByUserID(userID) {
         const id = await db.findOne(tbName, 'UserID', userID);
         const cartItems = await db.findByField('CartItems', 'CartID', id)
-        for(var cartItem of cartItems){
+        for (var cartItem of cartItems) {
             await CartItemDel(cartItem.ID);
         }
-        const rs = await db.del(tbName, 'ID',id);
+        const rs = await db.del(tbName, 'ID', id);
         return rs;
     }
-    static async Update(id, order){
+    static async Update(id, order) {
         const condition = {
             value: id,
             field: "ID"
@@ -35,10 +35,10 @@ module.exports = class Cart{
         const rs = await db.update(tbName, condition, order);
         return rs;
     }
-    static async GetByUserID(userID){
+    static async GetByUserID(userID) {
         const rs = await db.findOne(tbName, 'UserID', userID);
         return rs;
     }
-    
-    
+
+
 }
